4140a66aa6ea084ca8181b022c8e10e4b701e040
keiya0320 <keiya.man@icloud.com>
2020-08-26 14:34:02 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,19 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :email, uniquness: true
-  validates :email, inclusion: { in: ["@"]}
+  validates :email, uniqueness: true
   validates :password, presence: true
   validates :password, length: { minimum: 6 }
   VALID_PASSWORD_REGIX = /\A[a-z0-9]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGIX }
   validates :family_name, presence: true
   validates :first_name, presence: true
-  VALID_NAME_REGIX = /^[ぁ-んァ-ン一-龥]/
+  VALID_NAME_REGIX = /\A^[ぁ-んァ-ン一-龥]\z/
   validates :family_name, format: { with: VALID_NAME_REGIX }
   validates :first_name, format: { with: VALID_NAME_REGIX }
   validates :read_family, presence: true
   validates :read_first, presence: true
-  VALID_READ_REGIX = /^([ァ-ン]|ー)+$/
+  VALID_READ_REGIX = /\A^([ァ-ン]|ー)+$\z/
   validates :read_family, format: { with: VALID_READ_REGIX }
   validates :read_first, format: { with: VALID_READ_REGIX }
   validates :birth, presence: true
