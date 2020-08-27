69b82dda0dce3747ace31922742c165496ac8d10
keiya0320 <keiya.man@icloud.com>
2020-08-27 19:28:36 +0900

    js実装の準備

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:photo, :name, :explanation, :category, :condition, :postage_type, :prefectures, :preparation_days, :value, :user)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+end
