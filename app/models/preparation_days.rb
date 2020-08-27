9516ec30746b18db91af6c28b102be173433ac19
keiya0320 <keiya.man@icloud.com>
2020-08-27 19:29:26 +0900

    active hashの訂正

--- app/models/preparation_days.rb
+++ app/models/preparation_days.rb
@@ -1,4 +1,5 @@
-class Preparation_days < ActiveHash::Base
+class PreparationDays < ActiveHash::Base
   self.data = [
     {id: 1, name: '---'},{id: 2, name: '1~2日で発送'},{id: 3, name: '2~3日で発送'},{id: 4, name: '4~7日で発送'}
   ]
+end
