0462384d8b4c06a954f4ffce70ed3eaedc4e0c42
keiya0320 <keiya.man@icloud.com>
2020-08-27 19:30:09 +0900

    ビューファイルにプルダウンを実装する

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,17 +12,5 @@ class Product < ApplicationRecord
   validates :value, numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
   VALID_VALUE_REGIX = /\A[0-9]+\z/i
   validates :value, format: { with: VALID_VALUE_REGIX }
-  
 
 end
-t.text :photo, null: false
-t.string :name, null: false
-t.text :explanation, null: false
-t.integer :category, null: false
-t.integer :condition, null:false
-t.integer :postage_type, null:false
-t.integer :prefectures, null:false
-t.integer :preparation_days, null:false
-t.integer :value, null:false
-t.references :user, foreign_key: true
-t.timestamps
