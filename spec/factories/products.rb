b9524ef0df66216a921cc44d3a8d6c489686208d
keiya0320 <keiya.man@icloud.com>
2020-10-03 19:26:07 +0900

    update

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :product do
     name { Faker::Types.rb_string }
     explanation { Faker::Types.rb_string }
-    category { Faker::Number.between(from: 1, to: 11) }
-    condition { Faker::Number.between(from: 1, to: 7) }
-    postage_type { Faker::Number.between(from: 1, to: 3) }
-    prefecture { Faker::Number.between(from: 1, to: 47) }
-    preparation_days { Faker::Number.between(from: 1, to: 4) }
+    category_id { Faker::Number.between(from: 1, to: 11) }
+    condition_id { Faker::Number.between(from: 1, to: 7) }
+    postage_type_id { Faker::Number.between(from: 1, to: 3) }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    preparation_days_id { Faker::Number.between(from: 1, to: 4) }
     value { Faker::Number.number(digits: 5) }
   end
 end
